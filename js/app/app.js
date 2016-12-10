cac5db9e3f849f73b3f65dd2b97c5dd95e942ebc
Dom McKellar <dom.mckellar@gmail.com>
2016-12-09 16:43:37 -0800

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,53 @@
+// angular
+//     .module('app', ['ui.router'])
+//
+//     // NOTE: $urlRouterProvider has the responsibility of watching $location. When $location changes it runs through a list of rules one by one until a match is found. $urlRouterProvider is used behind the scenes anytime you specify a url in a state configuration.
+//     .config(function ($stateProvider, $urlRouterProvider){
+//       $stateProvider
+//         .state('home', {
+//             url: 'home',
+//             templateUrl: 'views/home.html'
+//         })
+//         .state('home.notifications', {
+//             url: 'notifications',
+//             templateUrl: 'views/home/notifications.html'
+//         })
+//         .state('home.timeline', {
+//             url: 'timeline',
+//             templateUrl: 'views/home/timeline.html'
+//         })
+//         .state('home.user', {
+//             url: 'user',
+//             templateUrl: 'views/home/user.html'
+//         })
+//
+//         // NOTE: fallback
+//         $urlRouterProvider.otherwise('/');
+//     });
+
+
+
+
 angular
-    .module('app', ['']);
+    .module('app', ['ui.router'])
+	.config(function ($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			.state('home', {
+				url: '/',
+				templateUrl: 'views/home.html'
+			})
+			.state('home.notifications', {
+				url: 'notifications',
+				templateUrl: 'views/home/notifications.html'
+			})
+			.state('home.timeline', {
+				url: 'timeline',
+				templateUrl: 'views/home/timeline.html'
+			})
+			.state('home.user', {
+				url: 'user',
+				templateUrl: 'views/home/user.html'
+			});
+
+		$urlRouterProvider.otherwise('/');
+	});
---
+++ js/app/controllers/UserController.js
@@ -0,0 +1,6 @@
+angular
+  .module('app')
+  .controller('UserController', function(user){
+    debugger
+    this.user = user.data;
+  });
